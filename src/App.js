c42befe11cbb4e576860c46d02fb15f36bb01fd8
oceanzove <zove888@gmail.com>
2024-01-02 19:45:44 +0500

    IT-KAMASUTRA 07

--- src/App.js
+++ src/App.js
@@ -2,26 +2,51 @@ import './App.css';
 
 
 const App = () => {
-    return (<div className="App">
-            <ul>
-                <li>
-                    <p>HTML</p>
-                    <input type={"checkbox"}/>
-                </li>
-                <li>
-                    <p>CSS</p>
-                    <input type={"checkbox"}/>
-                </li>
-                <li>
-                    <p>JS</p>
-                    <input type={"checkbox"}/>
-                </li>
-                <li>
-                    <p>React</p>
-                    <input type={"checkbox"}/>
-                </li>
-            </ul>
-        </div>);
+    return <>
+            <div>
+                first div
+                <Header></Header>
+            </div>
+            <div>
+                <Technologies></Technologies>
+            </div>
+        </>
+
+}
+
+const Header = () => {
+    return(
+        <div>
+            <a href='#s'>Home</a>
+            <a href='#s'>News</a>
+            <a href='#s'>Messages</a>
+        </div>
+    )
+}
+
+const Technologies = () => {
+  return (
+      <div>
+          <ul>
+              <li>
+                  <p>HTML</p>
+                  <input type={"checkbox"}/>
+              </li>
+              <li>
+                  <p>CSS</p>
+                  <input type={"checkbox"}/>
+              </li>
+              <li>
+                  <p>JS</p>
+                  <input type={"checkbox"}/>
+              </li>
+              <li>
+                  <p>React</p>
+                  <input type={"checkbox"}/>
+              </li>
+          </ul>
+      </div>
+  )
 }
 
 export default App;
