9aa083bd3cac62b511ce1bcc9d6fa6cc94f84f81
oceanzove <zove888@gmail.com>
2024-01-02 20:01:52 +0500

    Реализует разбивку на компонеты

--- src/App.js
+++ src/App.js
@@ -1,52 +1,19 @@
 import './App.css';
+import Technologies from "./components/Technologies";
+import Header from "./components/Header";
 
 
 const App = () => {
     return <>
             <div>
                 first div
-                <Header></Header>
+                <Header/>
             </div>
             <div>
-                <Technologies></Technologies>
+                <Technologies/>
             </div>
         </>
 
-}
-
-const Header = () => {
-    return(
-        <div>
-            <a href='#s'>Home</a>
-            <a href='#s'>News</a>
-            <a href='#s'>Messages</a>
-        </div>
-    )
-}
-
-const Technologies = () => {
-  return (
-      <div>
-          <ul>
-              <li>
-                  <p>HTML</p>
-                  <input type={"checkbox"}/>
-              </li>
-              <li>
-                  <p>CSS</p>
-                  <input type={"checkbox"}/>
-              </li>
-              <li>
-                  <p>JS</p>
-                  <input type={"checkbox"}/>
-              </li>
-              <li>
-                  <p>React</p>
-                  <input type={"checkbox"}/>
-              </li>
-          </ul>
-      </div>
-  )
 }
 
 export default App;
