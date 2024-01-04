4d51c1b60dffb9d396ab7dbf8e133797acd4857e
oceanzove <zove888@gmail.com>
2024-01-04 21:08:45 +0500

    IT-KAMASUTRA.COM начало разработки соц. сети

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import React from "react";
 import './App.css';
-import Technologies from "./components/Technologies";
-import Header from "./components/Header";
 
 
 const App = () => {
-    return <>
+    return <div className='app-wrapper'>
+        <header className='header'>
+            <img src='https://i.pinimg.com/564x/d1/3a/84/d13a842016c5e6d0a147ec341a4afaec.jpg'/>
+        </header>
+        <nav className='nav'>
             <div>
-                first div
-                <Header/>
+                Profile
             </div>
             <div>
-                <Technologies/>
+                Messages
             </div>
-        </>
+            <div>
+                Main content
+            </div>
+        </nav>
+        <div className='content'>
+
+        </div>
+    </div>
 
 }
 
