11341b51b7296deb049774bc1a3812ef663766c1
oceanzove <zove888@gmail.com>
2024-01-05 19:16:28 +0500

    начало разработки соц. сети 2

--- src/App.js
+++ src/App.js
@@ -9,17 +9,39 @@ const App = () => {
         </header>
         <nav className='nav'>
             <div>
-                Profile
+                <a> Profile</a>
             </div>
             <div>
-                Messages
+                <a> Messages</a>
             </div>
             <div>
-                Main content
+                <a> News</a>
             </div>
+            <div>
+                <a> Music </a>
+            </div>
+            <div>
+                <a> Settings</a>
+            </div>
+
         </nav>
         <div className='content'>
-
+            <div>
+                <img src="https://i.pinimg.com/736x/f0/39/a3/f039a33eea94d6efb65e42c758c190f6.jpg"/>
+            </div>
+            <div>
+                ava + desc
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+            </div>
+            <div>
+                <div> post 1</div>
+                <div> post 2</div>
+            </div>
         </div>
     </div>
 
