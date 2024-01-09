d1bfc4741c4f3fed201639cbcd56b80b2a74af60
oceanzove <zove888@gmail.com>
2024-01-09 13:58:45 +0500

    Реализует страницу Dialogs

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import classes from './Dialogs.module.css'
+
+const Dialogs = () => {
+    return (
+        <div className={classes.content}>
+            Dialogs
+        </div>
+    );
+}
+
+export default Dialogs
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import classes from './Profile.module.css'
 import MyPost from "./MyPost/MyPost";
 const Profile = () => {
     return (
-        <div className={classes.content}>
+        <div>
             <div>
                 <img src="https://i.pinimg.com/736x/f0/39/a3/f039a33eea94d6efb65e42c758c190f6.jpg"/>
             </div>
