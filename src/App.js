9109f6040b258000874942e3d9f7e3b55d3f3c5e
oceanzove <zove888@gmail.com>
2024-01-09 14:48:32 +0500

    Реализует маршрутизацию

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 import './App.css';
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+
 
 const App = () => {
-    return <div className='app-wrapper'>
-        <Header/>
-        <Navbar/>
-        <div className='app-wrapper-content'>
-            <Profile/>
-            {/*<Dialogs/>*/}
-        </div>
-    </div>
-
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path='/profile' element={<Profile/>}/>
+                        <Route path='/dialogs' element={<Dialogs/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
