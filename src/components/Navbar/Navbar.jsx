b2d027538d8718d9fed162e34a830618fd7a2088
oceanzove <zove888@gmail.com>
2024-01-12 13:39:43 +0500

    Добавляет в навбар друзей

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from './Navbar.module.css'
 import {NavLink, useLocation} from "react-router-dom";
 import {exact} from "prop-types";
+import Friends from "./Friends/Friends";
 
 const Navbar = () => {
     const location = useLocation();
@@ -31,6 +32,12 @@ const Navbar = () => {
             <NavLink to='/settings'
                      className={location.pathname === '/settings' ? styles.active : undefined}
             > Settings</NavLink>
+        </div>
+        <div className={styles.item}>
+            <NavLink to='/friends'
+                     className={location.pathname.match('/friends', exact) ? styles.active : undefined}
+            > Friends </NavLink>
+            <Friends />
         </div>
     </nav>)
 }
