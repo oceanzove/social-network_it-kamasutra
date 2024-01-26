a7a41b3f9b479d5d745e46a56beede9b74198240
oceanzove <zove888@gmail.com>
2024-01-26 21:47:15 +0500

    Исправляет ошибку отрисовки постов, путем копии состояния в ProfileReducer

--- src/index.js
+++ src/index.js
@@ -7,22 +7,13 @@ import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-let rerenderEntireTree = () => {
-    root.render(
-        <React.StrictMode>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </React.StrictMode>
-    );
-};
-
-
-rerenderEntireTree()
-
-store.subscribe( () => {
-    rerenderEntireTree();
-});
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
+);
 
 
 
